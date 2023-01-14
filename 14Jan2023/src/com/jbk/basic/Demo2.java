07551366b329fe94f4b9cfe85e5dabdaaf1e27ad
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2023-01-14 11:43:02 +0530

    14Jan Update

---
+++ 14Jan2023/src/com/jbk/basic/Demo2.java
@@ -0,0 +1,20 @@
+package com.jbk.basic;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		
+		WebDriver driver=new ChromeDriver();
+		
+		driver.get("https://www.rediff.com/");
+		
+
+	}
+
+}
